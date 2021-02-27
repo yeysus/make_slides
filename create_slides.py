a2ee93ddca5fa0f30a2ffe0af8baef060d7a46ad
Jesus del Valle <yeysus@gmail.com>
2021-02-27 16:43:02 +0100

    Starting new format

--- create_slides.py
+++ create_slides.py
@@ -17,6 +17,7 @@ import sys
 import time
 import pandas as pd
 import os, sys, subprocess
+import json
 
 from pptx import Presentation
 from pptx.dml.color import ColorFormat, RGBColor
@@ -49,19 +50,58 @@ nameOfExcelColumnWithYearFounded = 'Founded'
 nameOfExcelColumnWithHQCity = 'HQ'
 nameOfExcelColumnWithDescription = 'Description'
 
-# This title will appear on the top of every slide.
+# This title will appear on the top of every slide for certain formats.
 title = "Top30 BRITISH / WHERE to Make an Internship 2021"
 
-powerpoint = Presentation("My_Template_16x9_2.pptx")
-finalPowerPoint = "my_slides_2.pptx"
+powerpoint = Presentation ("examples/Template_16x9.pptx")
+finalPowerPoint = "examples/Slides.pptx"
 
 # The following measures are in inches.
-# titleHeight is the height reserved for the title.
+# titleHeight is the height reserved for the title, if any.
 # The title font size could be smaller.
 # The total size is 16 x 9.
-totalHeight = 9
-titleHeight = 1
-referenceHeight = 3
+
+# Example Layout: layout 1.
+# Title
+# 2 Pictures per slide, both left, with corresponding text on their right.
+#####
+# Title
+#--------
+#| pic1 | text1
+#--------
+#| pic2 | text2
+#--------
+layout1 = '{"layout":"1", \
+           "referenceHeight":3, \
+           "totalHeight":9, \
+           "nColumnsPerSlide":1, \
+           "textBoxPosition":"right", \
+           "pictureNameFont":40, \
+           "pictureDescriptionFont":20, \
+           "pictureUrlFont":12, \
+           "titleHeight":1}'
+
+layout2 = '{"layout":"2", \
+           "referenceHeight":3.65, \
+           "totalHeight":9, \
+           "nColumnsPerSlide":2, \
+           "textBoxPosition":"overlap", \
+           "pictureNameFont":28, \
+           "pictureDescriptionFont":20, \
+           "pictureUrlFont":12, \
+           "titleHeight":0}'
+
+layoutParameters = json.loads(layout2)
+
+totalHeight = layoutParameters ["totalHeight"]
+titleHeight = layoutParameters ["titleHeight"]
+referenceHeight = layoutParameters ["referenceHeight"]
+nColumnsPerSlide = layoutParameters ["nColumnsPerSlide"]
+textBoxPosition = layoutParameters ["textBoxPosition"]
+pictureNameFont = layoutParameters ["pictureNameFont"]
+pictureDescriptionFont = layoutParameters ["pictureDescriptionFont"]
+pictureUrlFont = layoutParameters ["pictureUrlFont"]
+
 pictureHeight = Inches (referenceHeight)
 pictureWidth = Inches (referenceHeight*9/5)
 # Total height is 9 inches. Reserving titleHeight for the title,
@@ -79,6 +119,8 @@ try:
   i = 0
   # iRow is the counter for the number of rows which will go into each slide.
   iRow = 0
+  # iColumn is the counter for the number of columns which will go into each slide.
+  iColumn = 0
   layout = powerpoint.slide_layouts[0]
   slide = powerpoint.slides.add_slide(layout)
   shape = slide.shapes
@@ -91,38 +133,44 @@ try:
     print (url)
     pictureFile = screenshotsDirectory + url + ".png"
 
-    if iRow == 0:
+    if iRow == 0 & iColumn == 0:
       slide = powerpoint.slides.add_slide(layout)
-      textBox = slide.shapes.add_textbox (Inches (0.05), Inches (0.0), Inches(14), Inches(1))
-      textFrame = textBox.text_frame
-      textFrame.word_wrap = True
-      # A text_frame always contains one paragraph.
-      p = textFrame.paragraphs[0]
-      p.font.size = Pt(40)
-      p.font.color.rgb = RGBColor(245, 223, 77)
-      p.text = title
+      if titleHeight > 0:
+        textBox = slide.shapes.add_textbox (Inches (0.05), Inches (0.0), Inches(14), Inches(1))
+        textFrame = textBox.text_frame
+        textFrame.word_wrap = True
+        # A text_frame always contains one paragraph.
+        p = textFrame.paragraphs[0]
+        p.font.size = Pt(40)
+        p.font.color.rgb = RGBColor(245, 223, 77)
+        p.text = title
 
     # 16:9 -> 3 columns each 5.33, 4 rows each 2.25
     # new height = (5/9) * new width
 
     # 13.3 x 7.5 inches -> 4.43 x 1.875
 
-    left = Inches (0)
-    top = Inches (titleHeight + (iRow * referenceHeight) + (iRow * titleHeight / 3))
+    left = Inches (iColumn * pictureWidth + (iColumn / 4.5))
+    top = Inches (titleHeight + (iRow * referenceHeight) + (iRow / 4.5))
 
     picture=slide.shapes.add_picture (pictureFile, left, top, width = pictureWidth, height = pictureHeight)
 
     # Add text.
     # From https://python-pptx.readthedocs.io/en/latest/user/text.html
 
-    j = 0
-
-    txBox = slide.shapes.add_textbox (left + pictureWidth, top, Inches(8), Inches(3))
+    if textBoxPosition == "right":
+      left = left + pictureWidth
+    elif textBoxPosition == "overlap":
+      left = left
+    txBox = slide.shapes.add_textbox (left, top, Inches(totalHeight - titleHeight), Inches(referenceHeight))
     tf = txBox.text_frame
     tf.word_wrap = True
+    fill = txBox.fill
+    fill.solid()
+    fill.fore_color.rgb = RGBColor (0, 0, 0)
 
     p = tf.add_paragraph()
-    p.font.size = Pt(40)
+    p.font.size = Pt(pictureNameFont)
     p.font.color.rgb = RGBColor(255, 255, 255)
     p.text = row [nameOfExcelColumnWithCompanyName]
     p = tf.add_paragraph()
@@ -133,16 +181,16 @@ try:
       rowFounded = ""
     else:
       rowFounded = "Founded: " + str (int (row [nameOfExcelColumnWithYearFounded])) + ". "
-    p.font.size = Pt(20)
+    p.font.size = Pt(pictureDescriptionFont)
     p.font.color.rgb = RGBColor(255, 255, 255)
     p.text = row [nameOfExcelColumnWithHQCity] + '. ' + rowFounded + row [nameOfExcelColumnWithDescription]
 
     p = tf.add_paragraph()
-    p.font.size = Pt(12)
+    p.font.size = Pt(pictureUrlFont)
     p.font.color.rgb = RGBColor(255, 255, 255)
     p.text = row [nameOfExcelColumnWithURLs]
 
-    j = j + 1
+    #j = j + 1
     # print ("Shape written: " + str (j))
     iRow = iRow + 1
     # print ("iRow: " + str(iRow))
